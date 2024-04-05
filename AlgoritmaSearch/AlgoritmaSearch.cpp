244548d12d572cf00238b22f56c3a0eba6e65748
darasdarmawan <darasyauqidarmawan@gmail.com>
2024-04-05 10:25:54 +0700

    kondisi if kedua pd step 5

--- AlgoritmaSearch/AlgoritmaSearch.cpp
+++ AlgoritmaSearch/AlgoritmaSearch.cpp
@@ -50,6 +50,13 @@ void LineSearch()
 			}
 			i++;
 		}
+		
+		if (i == n)
+			cout << "\n" << item << " not found in the array\n";
+		cout << "\nNumber of comparisons: " << ctr << endl;
+
+		cout << "\nContinue search (y/n): ";
+		cin >> ch;
 
 	}
 }
